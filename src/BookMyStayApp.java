9e5c31ae6aa9c7eaa8267a7e32034069acab75fe
mvrhemanth2006-droid <mvrhemanth2006@gmail.com>
2026-03-18 10:07:48 +0530

    UC7: Implemented AddOn services, CancellationService and integrated full booking system

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -4,23 +4,92 @@ public class BookMyStayApp {
 
         System.out.println("===== Book My Stay App =====");
 
-        // Create room objects
+
+
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
 
-        // Create inventory
         RoomInventory inventory = new RoomInventory();
-
-        // Search service
         RoomSearchService searchService = new RoomSearchService();
 
-        // Show available rooms
         searchService.searchAvailableRooms(
                 inventory,
                 singleRoom,
                 doubleRoom,
                 suiteRoom
         );
+
+
+
+        System.out.println("\nBooking Request Queue:");
+
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+
+            Reservation request = bookingQueue.getNextRequest();
+
+            System.out.println(
+                    request.getGuestName() +
+                            " requested " +
+                            request.getRoomType() +
+                            " room"
+            );
+        }
+
+
+
+        System.out.println("\nRoom Allocation:");
+
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        RoomAllocationService allocationService = new RoomAllocationService();
+
+        while (bookingQueue.hasPendingRequests()) {
+
+            Reservation request = bookingQueue.getNextRequest();
+
+            allocationService.allocateRoom(request, inventory);
+        }
+        System.out.println("\nAdd-On Services:");
+
+        AddOnServiceManager serviceManager = new AddOnServiceManager();
+
+
+        String res1 = "RES-101";
+        String res2 = "RES-102";
+
+
+        AddOnService wifi = new AddOnService("WiFi", 500);
+        AddOnService breakfast = new AddOnService("Breakfast", 300);
+        AddOnService spa = new AddOnService("Spa", 1500);
+
+
+        serviceManager.addService(res1, wifi);
+        serviceManager.addService(res1, breakfast);
+        serviceManager.addService(res2, spa);
+
+
+        serviceManager.displayServices(res1);
+        serviceManager.displayServices(res2);
+
+
+        System.out.println("Total cost for " + res1 + ": ₹" +
+                serviceManager.calculateTotalCost(res1));
+
+        System.out.println("Total cost for " + res2 + ": ₹" +
+                serviceManager.calculateTotalCost(res2));
     }
-}}
+}
